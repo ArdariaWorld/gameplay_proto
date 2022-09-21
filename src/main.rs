e327ebd6b2336ff5cf0bfeec9b9df3b5511231cf
jean-baptiste briant <jbbriant44@gmail.com>
2022-09-21 17:20:03 +0700

    FT - unit tests

--- src/main.rs
+++ src/main.rs
@@ -4,10 +4,10 @@ struct GreetTimer(Timer);
 
 impl Plugin for PopulationPlugin {
     fn build(&self, app: &mut App) {
-        app.insert_resource(GreetTimer(Timer::from_seconds(2.0, true)))
-        .add_startup_system(add_player)
-        .add_startup_system(add_monsters)
-        .add_system(combat_system);
+        app.insert_resource(GreetTimer(Timer::from_seconds(0.2, true)))
+            .add_startup_system(add_player)
+            .add_startup_system(add_monsters)
+            .add_system(combat_system);
     }
 }
 
@@ -24,39 +24,44 @@ struct Name(String);
 struct Stats {
     hp: f32,
     atk: f32,
-    def: f32
 }
-
 
 // System
-fn add_player(mut commands: Commands) {
-    commands.spawn()
-    .insert(Player)
-    .insert(Name("SirMashaa".to_string()))
-    .insert(Stats{ hp: 100f32, atk: 10f32, def: 0f32 });
+fn add_player(mut commands: Commands) -> () {
+    commands
+        .spawn()
+        .insert(Player)
+        .insert(Name("SirMashaa".to_string()))
+        .insert(Stats {
+            hp: 100f32,
+            atk: 10f32,
+        });
 }
 
-fn add_monsters(mut commands: Commands) {
-    commands.spawn()
-    .insert(Monster)
-    .insert(Name("Monster 1".to_string()))
-    .insert(Stats{ hp: 100f32, atk: 0f32, def: 5f32 });
+fn add_monsters(mut commands: Commands) -> () {
+    commands
+        .spawn()
+        .insert(Monster)
+        .insert(Name("Monster 1".to_string()))
+        .insert(Stats {
+            hp: 100f32,
+            atk: 0f32,
+        });
 }
 
-fn compute_new_hps(player_atk :f32, monster_hp: f32) -> f32 {
-    monster_hp - player_atk
+fn compute_new_hps(player_stats: &Stats, monster_stats: &Stats) -> f32 {
+    monster_stats.hp - player_stats.atk
 }
 
 // System
 fn combat_system(
     mut commands: Commands,
-    time: Res<Time>, 
-    mut timer: ResMut<GreetTimer>, 
-    player_query: Query<&Stats, With<Player>>, 
-    mut monsters_query: Query<(Entity, &Name, &mut Stats), Without<Player>>
+    time: Res<Time>,
+    mut timer: ResMut<GreetTimer>,
+    player_query: Query<&Stats, With<Player>>,
+    mut monsters_query: Query<(Entity, &Name, &mut Stats), Without<Player>>,
 ) {
     if timer.0.tick(time.delta()).just_finished() {
-
         let player_stats = player_query.single();
 
         if monsters_query.is_empty() {
@@ -64,25 +69,64 @@ fn combat_system(
         }
 
         for (entity_id, name, mut monster_stats) in monsters_query.iter_mut() {
-
             // Todo move into single function and unit test
-            monster_stats.hp = compute_new_hps(player_stats.atk, monster_stats.hp);
-            if monster_stats.hp <= 0.0 {
-                commands.entity(entity_id).despawn();
-                eprintln!("Monster {} has been killed", name.0);
-            }
-
-
-            eprintln!("Monster {} has {} HP.", name.0, monster_stats.hp);
+            attack(
+                &mut commands,
+                entity_id,
+                &mut monster_stats,
+                player_stats,
+                name,
+            );
         }
     }
 }
 
-// System
+fn despawn(commands: &mut Commands, entity_id: Entity) -> () {
+    commands.entity(entity_id).despawn();
+}
+
+// Attack system
+// check it despawn a dead ennemy
+fn attack(
+    commands: &mut Commands,
+    entity_id: Entity,
+    monster_stats: &mut Stats,
+    player_stats: &Stats,
+    monster_name: &Name,
+) -> () {
+    monster_stats.hp = compute_new_hps(&player_stats, &monster_stats);
+    if monster_stats.hp <= 0.0 {
+        despawn(commands, entity_id);
+        // commands.entity(entity_id).despawn();
+        eprintln!("Monster {} has been killed", monster_name.0);
+    }
+    eprintln!("Monster {} has {} HP.", monster_name.0, monster_stats.hp);
+}
 
 fn main() {
     App::new()
         .add_plugins(MinimalPlugins)
         .add_plugin(PopulationPlugin)
         .run();
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::*;
+
+    #[test]
+    fn test_attack() {
+        let player_stats = Stats {
+            hp: 100f32,
+            atk: 10f32,
+        };
+
+        let monster_stats = Stats {
+            hp: 100f32,
+            atk: 100f32,
+        };
+
+        let new_monster_hps = compute_new_hps(&player_stats, &monster_stats);
+        assert_eq!(new_monster_hps, 90f32);
+    }
 }
