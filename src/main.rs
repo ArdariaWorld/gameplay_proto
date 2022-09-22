1df27702d43e6852083abb8af281dc1bdbe2447f
jean-baptiste briant <jbbriant44@gmail.com>
2022-09-22 23:17:26 +0700

    FT - detect mouse click and move player

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,79 @@
-use bevy::prelude::*;
-pub struct PopulationPlugin;
-struct GreetTimer(Timer);
+#![allow(dead_code)]
+mod game_cake;
+use bevy::{
+    input::{
+        mouse::{MouseButtonInput, MouseMotion},
+        ButtonState,
+    },
+    prelude::*,
+};
+use game_cake::main_bis;
+use rand::Rng;
 
+struct GreetTimer(Timer);
+struct LocationTimer(Timer);
+
+pub struct PopulationPlugin;
 impl Plugin for PopulationPlugin {
     fn build(&self, app: &mut App) {
         app.insert_resource(GreetTimer(Timer::from_seconds(0.2, true)))
             .add_startup_system(add_player)
-            .add_startup_system(add_monsters)
-            .add_system(combat_system);
+            .add_startup_system(add_monsters);
+    }
+}
+
+pub struct LocationPlugin;
+impl Plugin for LocationPlugin {
+    fn build(&self, app: &mut App) {
+        app.add_system(location_system);
+    }
+}
+
+fn location_system(
+    time: Res<Time>,
+    mut creatures_query: Query<(&mut Location, &mut Transform), With<Creature>>,
+) {
+    for (mut location, mut transform) in creatures_query.iter_mut() {
+        // Update location if entity have a destination
+        if let (Some(destination), Some(position)) = (location.destination, location.position) {
+            // compute vector from position to destination
+            let delta_v = destination - position;
+            println!("{:?}", delta_v);
+            location.position = Some(position + delta_v.normalize() * 100.0 * time.delta_seconds());
+            // reduce vector to maximum velocity
+            // apply new vector to position to get new_posittion
+        }
+        // transform.translation.x += 10.0 * time.delta_seconds();
+
+        // Update sprite transform from entity position
+        if let Some(position) = location.position {
+            transform.translation = position.extend(1.0);
+        }
+    }
+}
+
+fn handle_mouse_click(
+    mut mouse_button_input_events: EventReader<MouseButtonInput>,
+    windows: Res<Windows>,
+    mut player_query: Query<&mut Location, With<Player>>,
+) {
+    // eprintln!("Mouse event system {}", player_query.is_empty());
+    for event in mouse_button_input_events.iter() {
+        if event.state == ButtonState::Pressed {
+            let win = windows.get_primary().expect("no primary window");
+            // println!("{:?}", win.cursor_position());
+            if let (Ok(mut location), Some(cursor_position)) =
+                (player_query.get_single_mut(), win.cursor_position())
+            {
+                location.destination = Some(cursor_position);
+            }
+        }
     }
 }
 
 fn compute_new_hps(player_stats: &Stats, monster_stats: &Stats) -> f32 {
     monster_stats.hp - player_stats.atk
 }
-
 // System
 fn combat_system(
     mut commands: Commands,
@@ -144,27 +203,6 @@ struct PlayerBundle {
     // inputs
 }
 
-enum Creature {
-    Human,
-    Monster,
-}
-
-impl Creature {
-    fn color(&self) -> Color {
-        match self {
-            Creature::Human => Color::rgb(0.4, 0.7, 0.1),
-            Creature::Monster => Color::rgb(0.1, 0.7, 0.4),
-        }
-    }
-
-    fn size(&self) -> Vec3 {
-        match self {
-            Creature::Human => Vec2::new(17.0, 40.0).extend(1.0),
-            Creature::Monster => Vec2::new(25.0, 35.0).extend(1.0),
-        }
-    }
-}
-
 #[derive(Default, Component)]
 struct Location {
     destination: Option<Vec2>,
@@ -196,7 +234,7 @@ impl Location {
     }
 }
 
-#[derive(Default, Bundle, Component)]
+#[derive(Default, Bundle)]
 struct CreatureBundle {
     stats: Stats,
     name: Name,
@@ -207,7 +245,7 @@ struct CreatureBundle {
 }
 
 impl CreatureBundle {
-    fn new(creature: Creature, name_str: String, hp: f32, atk: f32) -> CreatureBundle {
+    fn new(creature: CreatureType, name_str: String, hp: f32, atk: f32) -> CreatureBundle {
         CreatureBundle {
             stats: Stats { hp, atk },
             name: Name(name_str),
@@ -234,32 +272,59 @@ struct Monster;
 #[derive(Component)]
 struct Player;
 
+enum CreatureType {
+    Human,
+    Monster,
+}
+
+#[derive(Default, Component)]
+struct Creature;
+
+impl CreatureType {
+    fn color(&self) -> Color {
+        match self {
+            CreatureType::Human => Color::rgb(0.4, 0.7, 0.1),
+            CreatureType::Monster => Color::rgb(0.1, 0.7, 0.4),
+        }
+    }
+
+    fn size(&self) -> Vec3 {
+        match self {
+            CreatureType::Human => Vec2::new(17.0, 40.0).extend(1.0),
+            CreatureType::Monster => Vec2::new(25.0, 35.0).extend(1.0),
+        }
+    }
+}
+
 // System
 fn add_player(mut commands: Commands) -> () {
     commands
         .spawn_bundle(PlayerBundle {
-            creature: CreatureBundle::new(Creature::Human, "Jbb".to_string(), 100.0, 20.0),
+            creature: CreatureBundle::new(CreatureType::Human, "Jbb".to_string(), 100.0, 20.0),
         })
+        .insert(Creature)
         .insert(Player);
 }
 
 fn add_monsters(mut commands: Commands) -> () {
     commands
         .spawn_bundle(CreatureBundle::new(
-            Creature::Monster,
+            CreatureType::Monster,
             "Monster 1".to_string(),
             90.0,
             10.0,
         ))
+        .insert(Creature)
         .insert(Monster);
 }
 
 fn main() {
-    // main_bis();
     App::new()
         .init_resource::<Game>()
         .add_plugins(DefaultPlugins)
         .add_plugin(PopulationPlugin)
+        .add_system(handle_mouse_click)
+        .add_plugin(LocationPlugin)
         .add_startup_system(init_world_map)
         .add_system(bevy::window::close_on_esc)
         .run();
@@ -267,6 +332,8 @@ fn main() {
 
 #[cfg(test)]
 mod tests {
+    use std::time::Duration;
+
     use crate::*;
 
     #[test]
@@ -283,5 +350,38 @@ mod tests {
 
         let new_monster_hps = compute_new_hps(&player_stats, &monster_stats);
         assert_eq!(new_monster_hps, 90f32);
+    }
+
+    #[test]
+    fn did_update_sprite_transforms() {
+        // Setup app
+        let mut app = App::new();
+
+        app.init_resource::<Game>();
+        app.add_plugins(MinimalPlugins);
+        app.add_plugin(PopulationPlugin);
+        app.add_plugin(LocationPlugin);
+        app.add_startup_system(init_world_map);
+
+        // Update system once
+        app.update();
+
+        // Query creatures after the update
+        let mut creatures_query = app
+            .world
+            .query_filtered::<(&Location, &Transform), With<Creature>>();
+
+        // Query should not be empty
+        let is_empty = creatures_query.is_empty(&app.world, 0, 0);
+        assert_eq!(is_empty, false);
+
+        // Expect transform to have been updated according to position
+        for (location, transform) in creatures_query.iter(&app.world) {
+            // Every transform.translation should have been updated
+            assert_eq!(
+                location.position.unwrap().extend(1.0),
+                transform.translation
+            );
+        }
     }
 }
